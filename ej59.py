934f1d75a6543862f530912c169ff7f12316e053
JaumeLloretRubio <al425988@uji.es>
2022-09-29 17:47:35 +0200

    Añadidos archivos de prueba

---
+++ ej59.py
@@ -0,0 +1,11 @@
+x = int(input('Edad persona 1: '))
+y = int(input('Edad persona 2: '))
+
+if x > y:
+    print('La persona 1 es más mayor')
+    
+elif x < y:
+    print('La persona 2 es más mayor')
+
+else:
+    print('Tienen la misma edad')
